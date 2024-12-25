88172990909bff214f7a2884707c60fed74d3a1d
erbiao3k <1041876270@qq.com>
2024-12-25 16:51:04 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -11,7 +11,7 @@ var (
 		"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=333"}
 
 	PlatformInfo = map[string]string{
-		"阿里云账号：新欧达":         "xod_aly_1755048621309203",
-		"阿里云账号：newouda.com": "xod_aly_1114624318124043",
+		"阿里云账号：账号1":         "xod_aly_1755048621309603",
+		"阿里云账号：账号2": "xod_aly_1114624318124073",
 	}
 )
